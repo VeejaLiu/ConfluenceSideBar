1851646811dfe8f1cd5330733d26eac8f7646dce
VeejaLiu <veejaliu@gmail.com>
2025-03-14 15:42:08 +0800

    refactor: simplify Popup component and add ticket navigation feature

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,62 +1,60 @@
 import '@src/Popup.css';
-import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
-import { exampleThemeStorage } from '@extension/storage';
-import { t } from '@extension/i18n';
-import { ToggleButton } from '@extension/ui';
-
-const notificationOptions = {
-  type: 'basic',
-  iconUrl: chrome.runtime.getURL('icon-34.png'),
-  title: 'Injecting content script error',
-  message: 'You cannot inject script here!',
-} as const;
+import { withErrorBoundary, withSuspense } from '@extension/shared';
+import { useEffect, useRef, useState } from 'react';
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
-  const isLight = theme === 'light';
-  const logo = isLight ? 'popup/logo_vertical.svg' : 'popup/logo_vertical_dark.svg';
-  const goGithubSite = () =>
-    chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
+  const [ticketNumber, setTicketNumber] = useState<string>('');
+  const prefix = 'hiretual';
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const injectContentScript = async () => {
-    const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
+  useEffect(() => {
+    // Focus the input field when the component mounts
+    inputRef.current?.focus();
+  }, []);
 
-    if (tab.url!.startsWith('about:') || tab.url!.startsWith('chrome:')) {
-      chrome.notifications.create('inject-error', notificationOptions);
+  const goToTicket = () => {
+    if (ticketNumber.trim()) {
+      chrome.tabs.create({ url: `https://${prefix}.atlassian.net/browse/${ticketNumber}` });
     }
-
-    await chrome.scripting
-      .executeScript({
-        target: { tabId: tab.id! },
-        files: ['/content-runtime/index.iife.js'],
-      })
-      .catch(err => {
-        // Handling errors related to other paths
-        if (err.message.includes('Cannot access a chrome:// URL')) {
-          chrome.notifications.create('inject-error', notificationOptions);
-        }
-      });
   };
 
   return (
-    <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
-      <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
-        <button onClick={goGithubSite}>
-          <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
+    <div className={`App`}>
+      {/* Go to any Ticket -------------- start */}
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          value={ticketNumber}
+          onChange={e => setTicketNumber(e.target.value)}
+          onKeyDown={e => {
+            if (e.key === 'Enter') {
+              goToTicket();
+            }
+          }}
+          placeholder="Enter ticket number"
+          className={`border-2 rounded-md p-1 border-gray-400`}
+        />
+        <button className={`bg-blue-500 text-white rounded-md p-2 m-2`} onClick={goToTicket}>
+          Go to Ticket
         </button>
-        <p>
-          Edit <code>pages/popup/src/Popup.tsx</code>
-        </p>
-        <button
-          className={
-            'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-            (isLight ? 'bg-blue-200 text-black' : 'bg-gray-700 text-white')
-          }
-          onClick={injectContentScript}>
-          Click to inject Content Script
-        </button>
-        <ToggleButton>{t('toggleTheme')}</ToggleButton>
-      </header>
+      </div>
+      {/* Go to any Ticket -------------- end */}
+
+      <div>
+        If you have any issue or suggestion, please visit{' '}
+        <a
+          className="text-blue-400 font-bold hover:underline"
+          href="https://github.com/VeejaLiu/ConfluenceSideNavbar/issues"
+          target="_blank">
+          github issue page
+        </a>{' '}
+        to report it.
+      </div>
+
+      <div className={'text-gray-500 text-sm mt-4'}>
+        Explore with ease through our intuitive side navigation bar on Confluence!
+      </div>
     </div>
   );
 };
